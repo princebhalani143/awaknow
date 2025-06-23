03c8ac53d2e7ab0373e2db71dc4e9244d0e7611a
Group Of Oceninfo <princebhalani143@gmail.com>
2025-06-23 16:49:59 +0530

    Updated UsageMeter.tsx

--- src/components/Subscription/UsageMeter.tsx
+++ src/components/Subscription/UsageMeter.tsx
@@ -48,6 +48,7 @@ export const UsageMeter: React.FC<UsageMeterProps> = ({ limits, subscription }) 
             </div>
             <span className="text-sm text-neutral-600">
               {limits.tavusMinutes} / {subscription.tavus_minutes_limit}
+              <p>Each additional minute will incur a charge of $0.20 USD, billed to your card.</p>
             </span>
           </div>
           <div className="w-full bg-neutral-200 rounded-full h-2">
