d2c550dbc1d169f5051431af05e3785f146ed54f
Group Of Oceninfo <princebhalani143@gmail.com>
2025-06-28 15:53:47 +0530

    Updated Landing.tsx

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -414,9 +414,9 @@ export const Landing: React.FC = () => {
                   <AnimatePresence mode="wait">
                     <motion.div
                       key={currentSlide}
-                      initial={{ opacity: 0, scale: 0.6 }}
+                      initial={{ opacity: 0, scale: 0.5 }}
                       animate={{ opacity: 1, scale: 1 }}
-                      exit={{ opacity: 0, scale: 0.6 }}
+                      exit={{ opacity: 0, scale: 0.5 }}
                       transition={{ duration: 0.5, ease: "easeInOut" }}
                       className="relative"
                     >
